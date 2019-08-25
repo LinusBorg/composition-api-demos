9dab2a608cfa8427758b0e8a686480507f5ec1f1
Thorsten <t.luenborg@googlemail.com>
2019-08-25 20:05:42 +0200

    feat: add Upload View to Navigation

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/posts">Infinite Posts</router-link>
+      <router-link to="/posts">Infinite Posts</router-link> |
+      <router-link to="/upload">Upload Dropzone</router-link>
     </div>
     <router-view />
   </div>
