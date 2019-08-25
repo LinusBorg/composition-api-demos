f7e05fdec5331dd2c5cf05068b946e8c1e184aea
Thorsten <t.luenborg@googlemail.com>
2019-08-25 20:38:11 +0200

    feat: add tailwindcss for styling of examples

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import FnApi from '@vue/composition-api'
 
+import './assets/main.css'
+
 Vue.use(FnApi)
 Vue.config.productionTip = false
 
