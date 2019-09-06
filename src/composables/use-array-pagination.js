d6c115c6d18fcea8467e71e0ee4253b56d0f68fb
Thorsten <t.luenborg@googlemail.com>
2019-09-06 22:02:51 +0200

    paginate a local array

---
+++ src/composables/use-array-pagination.js
@@ -0,0 +1,19 @@
+import usePagination from './use-pagination'
+import { ref, watch } from '@vue/composition-api'
+
+export default function useArrayPagination(arrayRef = ref([])) {
+  const pagination = usePagination(undefined, arrayRef.value.length)
+
+  const result = ref([])
+
+  watch(() => {
+    const array = arrayRef.value
+    if (!Array.isArray(array)) return
+    result.value = array.slice(
+      pagination.offset.value,
+      pagination.offset.value + pagination.perPage.value
+    )
+  })
+
+  return result
+}
