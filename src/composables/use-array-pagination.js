2d4a6bd9e0e8c7c109257668b592d2cd9f4285a7
Thorsten Lünborg <t.luenborg@googlemail.com>
2019-09-10 11:24:50 +0200

    fix: pass total as computed ref.

--- src/composables/use-array-pagination.js
+++ src/composables/use-array-pagination.js
@@ -2,7 +2,7 @@ import usePagination from './use-pagination'
 import { ref, watch } from '@vue/composition-api'
 
 export default function useArrayPagination(arrayRef = ref([])) {
-  const pagination = usePagination(undefined, arrayRef.value.length)
+  const pagination = usePagination(undefined, computed(() => arrayRef.value.length))
 
   const result = ref([])
 
