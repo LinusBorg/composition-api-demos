cf6fa0f2e020ecac143410d0404e55af56dd438d
Thorsten Lünborg <t.luenborg@googlemail.com>
2019-09-11 09:51:47 +0200

    fix: default argument makes no sense

--- src/composables/use-array-pagination.js
+++ src/composables/use-array-pagination.js
@@ -1,7 +1,7 @@
 import usePagination from './use-pagination'
 import { ref, watch } from '@vue/composition-api'
 
-export default function useArrayPagination(arrayRef = ref([])) {
+export default function useArrayPagination(arrayRef) {
   const pagination = usePagination(undefined, computed(() => arrayRef.value.length))
 
   const result = ref([])
