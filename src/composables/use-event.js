c330082595b99583b283e4294ff6cdcab2602ebe
Thorsten <t.luenborg@googlemail.com>
2019-09-21 15:32:10 +0200

    fix: should not treat el as a Ref since its unwrapped

--- src/composables/use-event.js
+++ src/composables/use-event.js
@@ -2,9 +2,9 @@ import { onMounted, onBeforeDestroy, ref } from '@vue/composition-api'
 import { unwrap } from '@/composables/utils'
 export default function useEvent(el = ref(document), name, handler) {
   el = unwrap(el)
-  const remove = () => el.value && el.value.removeEventListener(name, handler)
+  const remove = () => el && el.removeEventListener(name, handler)
 
-  onMounted(() => el.value && el.value.addEventListener(name, handler))
+  onMounted(() => el && el.addEventListener(name, handler))
   onBeforeDestroy(remove)
 
   return remove
